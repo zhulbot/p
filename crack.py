4dbe598613cfef7725c947e9aeb80cec4d481ee7
zhulbot <126550155+zhulbot@users.noreply.github.com>
2023-03-07 10:55:51 +0700

    Update crack.py

--- crack.py
+++ crack.py
@@ -307,8 +307,8 @@ def passwrdh():
 						pwv.append(frs+'1234')
 						pwv.append(frs+'12345')
 						pwv.append(frs+'123456')
-                        				pwv.append('indonesia')
-                        				pwv.append('bismillah')
+                        			pwv.append('indonesia')
+                        			pwv.append('bismillah')
 				else:
 					if len(frs)<3:
 						pwv.append(nmf)
@@ -319,8 +319,8 @@ def passwrdh():
 						pwv.append(frs+'1234')
 						pwv.append(frs+'12345')
 						pwv.append(frs+'123456')
-                        				pwv.append('indonesia')
-                       					pwv.append('bismillah')
+                        			pwv.append('indonesia')
+                       				pwv.append('bismillah')
 				if '><basari><' in pwt:
 					for xpwn in pwn:
 						pwv.append(xpwn)
