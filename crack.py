00181f3da4c952e41d10c0a6db38ca9b293b96f1
zhulbot <126550155+zhulbot@users.noreply.github.com>
2023-03-17 13:22:19 +0700

    Update crack.py

--- crack.py
+++ crack.py
@@ -118,7 +118,7 @@ def login_baz():
 		cok = open('.cookiesakun.txt','r').read()
 		tokenefb.append(token)
 		try:
-			gerap = requests.get('https://graph.facebook.com/v16.0/me?fields=id%2Cname&access_token='+tokenefb[0], cookies={'cookie':cok})
+			gerap = requests.get('https://graph.facebook.com/v15.0/me/?fields=id,name&access_token='+tokenefb[0], cookies={'cookie':cok})
 			nteng = json.loads(gerap.text)['id']
 			menu(nteng)
 		except KeyError:
